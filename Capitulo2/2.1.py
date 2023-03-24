d35763c0e915edb69221b3130655181d6f5ec901
DuduOliiver <eduardoliveira.11@hotmail.com>
2023-03-24 09:47:47 -0300

    capitulo2

---
+++ Capitulo2/2.1.py
@@ -0,0 +1,10 @@
+"""
+Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+10 + 20 × 30
+42 ÷ 30
+(94 + 2) × 6 - 1
+"""
+
+print(10 + 20 * 30)
+print(4 ** 2 / 30)
+print((9 ** 4 + 2) * 6 - 1)
